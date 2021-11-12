908140de821686bd3ebc87ac5942ad2025b70bb0
getwaroo <gavishetwaroo@gmail.com>
2021-11-12 10:15:52 +0400

    commit(1)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        //1st commit
+        //initial commit
         Student s = new Student(1,"james","james@gmail.com");
         System.out.println(s.display());
+
+        //1st commit
+        System.out.println("Hello world");
     }
 }
